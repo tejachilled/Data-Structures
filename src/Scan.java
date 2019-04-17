5e00bf9584fe76f6a7bca2d603ce7ca3881a1ca0
Ravi thutari <tejachilled@gmail.com>
2019-04-17 18:51:00 -0400

    testing merges

--- src/Scan.java
+++ src/Scan.java
@@ -28,6 +28,7 @@ public class Scan {
 //			System.out.println("number of words: "+words);
 //			System.out.println("number pf chars: "+chars);
 //		}
+// this is for testing branches  m2test
 		
 		String s1 = "Hello";
 		m1(s1);
